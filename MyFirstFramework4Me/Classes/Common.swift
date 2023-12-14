9723651a9a6bfeeedcfe49e4331b3975d2b65801
Al Dockett <al.dockett@gmail.com>
2023-12-14 12:32:55 -0500

    first commit

---
+++ MyFirstFramework4Me/Classes/Common.swift
@@ -0,0 +1,20 @@
+//
+//  Common.swift
+//  MyFirstFramework4Me
+//
+//  Created by Al Dockett on 12/14/23.
+//
+
+import Foundation
+
+public class Common{
+    
+    public static func doSomething(){
+        
+        print("I'M DOING GOOD THINGS......")
+        
+        
+    }
+    
+    
+}
---
+++ MyFirstFramework4Me/MyFirstFramework4Me.h
@@ -0,0 +1,18 @@
+//
+//  MyFirstFramework4Me.h
+//  MyFirstFramework4Me
+//
+//  Created by Al Dockett on 12/14/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstFramework4Me.
+FOUNDATION_EXPORT double MyFirstFramework4MeVersionNumber;
+
+//! Project version string for MyFirstFramework4Me.
+FOUNDATION_EXPORT const unsigned char MyFirstFramework4MeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstFramework4Me/PublicHeader.h>
+
+
